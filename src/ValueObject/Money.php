cae279a3354da8c0070b163fa61fbcb9b6138c74
Jakub Wietecha <estissy@gmail.com>
2026-01-06 16:28:26 +0100

    Add money value object

---
+++ src/ValueObject/Money.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EstissySystems\PhpCommon\ValueObject;
+
+use Ds\Hashable;
+use LogicException;
+use RoundingMode;
+
+/**
+ * Represents money, negative values available. The amount is stored in the smallest unit as an integer.
+ */
+readonly class Money implements Hashable
+{
+    private int $amount;
+    private Currency $currency;
+    private string $hash;
+
+    private function __construct(int $amount, Currency $currency)
+    {
+        $this->amount = $amount;
+        $this->currency = $currency;
+        $this->hash = hash('sha256', self::class . $amount . $currency->value);
+    }
+
+    /**
+     * @param int $amount The amount in the smallest unit as an integer
+     */
+    public static function fromAmountAndCurrency(int $amount, Currency $currency): Money
+    {
+        return new Money($amount, $currency);
+    }
+
+    public function toString(): string
+    {
+        $divisor = 10 ** $this->currency()->getDecimals();
+        $dividedAmount = $this->amount / $divisor;
+
+        return sprintf("%.{$this->currency()->getDecimals()}f %s", $dividedAmount, $this->currency()->value);
+    }
+
+    public function currency(): Currency
+    {
+        return $this->currency;
+    }
+
+    public function equals($obj): bool
+    {
+        if (($obj instanceof Hashable) === false) {
+            return false;
+        }
+
+        return $this->hash === $obj->hash();
+    }
+
+    public function hash(): string
+    {
+        return $this->hash;
+    }
+
+    public function add(Money $money): Money
+    {
+        if ($this->currency() !== $money->currency()) {
+            throw new LogicException(
+                "Error while adding money. Money currency {$this->currency()->value} is not equal added money currency {$money->currency()->value}."
+            );
+        }
+
+        return new Money($this->amount() + $money->amount(), $this->currency());
+    }
+
+    public function amount(): int
+    {
+        return $this->amount;
+    }
+
+    public function subtract(Money $money): Money
+    {
+        if ($this->currency() !== $money->currency()) {
+            throw new LogicException(
+                "Error while subtracting money. Money currency {$this->currency()->value} is not equal subtracted money currency {$money->currency()->value}."
+            );
+        }
+
+        return new Money($this->amount() - $money->amount(), $this->currency());
+    }
+
+    public function multiply(float $multiplier, RoundingMode $roundingMode = RoundingMode::HalfAwayFromZero): Money
+    {
+        $newAmount = (int)round($this->amount() * $multiplier, 0, $roundingMode);
+
+        return new Money($newAmount, $this->currency());
+    }
+
+    public function divide(float $divisor, RoundingMode $roundingMode = RoundingMode::HalfAwayFromZero): Money
+    {
+        $newAmount = (int)round($this->amount() / $divisor, 0, $roundingMode);
+
+        return new Money($newAmount, $this->currency());
+    }
+
+    public function convert(
+        Currency $targetCurrency,
+        float $conversionRate,
+        RoundingMode $roundingMode = RoundingMode::HalfAwayFromZero
+    ): Money {
+        $newAmount = (int)round($this->amount() * $conversionRate, 0, $roundingMode);
+
+        return new Money($newAmount, $targetCurrency);
+    }
+}
---
+++ tests/ValueObject/MoneyTest.php
@@ -0,0 +1,166 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ValueObject;
+
+use EstissySystems\PhpCommon\ValueObject\Currency;
+use EstissySystems\PhpCommon\ValueObject\Money;
+use LogicException;
+use PHPUnit\Framework\TestCase;
+
+class MoneyTest extends TestCase
+{
+    public function testToStringShouldReturnCorrectMoneyString(): void
+    {
+        $money = Money::fromAmountAndCurrency(15000, Currency::PLN);
+
+        self::assertSame('150.00 PLN', $money->toString());
+
+        $money = Money::fromAmountAndCurrency(15005, Currency::PLN);
+
+        self::assertSame('150.05 PLN', $money->toString());
+
+        $money = Money::fromAmountAndCurrency(0, Currency::PLN);
+
+        self::assertSame('0.00 PLN', $money->toString());
+
+        $money = Money::fromAmountAndCurrency(14, Currency::PLN);
+
+        self::assertSame('0.14 PLN', $money->toString());
+
+        $money = Money::fromAmountAndCurrency(-15005, Currency::PLN);
+
+        self::assertSame('-150.05 PLN', $money->toString());
+
+        $money = Money::fromAmountAndCurrency(15000, Currency::JPY);
+
+        self::assertSame('15000 JPY', $money->toString());
+    }
+
+    public function testEqualsShouldReturnTrueWhenAmountAndCurrencyOfProvidedMoneyIsEqual(): void
+    {
+        $firstMoney = Money::fromAmountAndCurrency(15000, Currency::PLN);
+        $secondMoney = Money::fromAmountAndCurrency(15000, Currency::PLN);
+
+        self::assertTrue($firstMoney->equals($secondMoney));
+    }
+
+    public function testEqualsShouldReturnFalseWhenAmountOrCurrencyOfProvidedMoneyIsNotEqual(): void
+    {
+        $firstMoney = Money::fromAmountAndCurrency(15000, Currency::PLN);
+        $secondMoney = Money::fromAmountAndCurrency(15005, Currency::PLN);
+
+        self::assertFalse($firstMoney->equals($secondMoney));
+
+        $firstMoney = Money::fromAmountAndCurrency(15000, Currency::PLN);
+        $secondMoney = Money::fromAmountAndCurrency(15000, Currency::USD);
+
+        self::assertFalse($firstMoney->equals($secondMoney));
+
+        $firstMoney = Money::fromAmountAndCurrency(15000, Currency::PLN);
+        $secondMoney = Money::fromAmountAndCurrency(15005, Currency::USD);
+
+        self::assertFalse($firstMoney->equals($secondMoney));
+    }
+
+    public function testAddShouldAddAmountsAndKeepCurrencyWhenMoneyWithSameCurrencyProvided(): void
+    {
+        $firstMoney = Money::fromAmountAndCurrency(15000, Currency::PLN);
+        $secondMoney = Money::fromAmountAndCurrency(15005, Currency::PLN);
+
+        $newMoney = $firstMoney->add($secondMoney);
+
+        self::assertSame(30005, $newMoney->amount());
+        self::assertSame(Currency::PLN, $newMoney->currency());
+    }
+
+    public function testAddShouldThrowExceptionWhenMoneyWithDifferentCurrencyProvided(): void
+    {
+        $firstMoney = Money::fromAmountAndCurrency(15000, Currency::PLN);
+        $secondMoney = Money::fromAmountAndCurrency(15005, Currency::USD);
+
+        $this->expectException(LogicException::class);
+
+        $firstMoney->add($secondMoney);
+    }
+
+    public function testSubtractShouldSubtractAmountsAndKeepCurrencyWhenMoneyWithSameCurrencyProvided(): void
+    {
+        $firstMoney = Money::fromAmountAndCurrency(15000, Currency::PLN);
+        $secondMoney = Money::fromAmountAndCurrency(15005, Currency::PLN);
+
+        $newMoney = $firstMoney->subtract($secondMoney);
+
+        self::assertSame(-5, $newMoney->amount());
+        self::assertSame(Currency::PLN, $newMoney->currency());
+    }
+
+    public function testSubtractShouldThrowExceptionWhenMoneyWithDifferentCurrencyProvided(): void
+    {
+        $firstMoney = Money::fromAmountAndCurrency(15000, Currency::PLN);
+        $secondMoney = Money::fromAmountAndCurrency(15005, Currency::USD);
+
+        $this->expectException(LogicException::class);
+
+        $firstMoney->subtract($secondMoney);
+    }
+
+    public function testMultiplyShouldMultiplyAmountAndKeepCurrency(): void
+    {
+        $firstMoney = Money::fromAmountAndCurrency(15000, Currency::PLN);
+
+        $newMoney = $firstMoney->multiply(1.2);
+
+        self::assertSame(18000, $newMoney->amount());
+        self::assertSame(Currency::PLN, $newMoney->currency());
+
+        $firstMoney = Money::fromAmountAndCurrency(15005, Currency::PLN);
+
+        $newMoney = $firstMoney->multiply(1.33);
+
+        self::assertSame(19957, $newMoney->amount());
+        self::assertSame(Currency::PLN, $newMoney->currency());
+
+        $firstMoney = Money::fromAmountAndCurrency(15005, Currency::PLN);
+
+        $newMoney = $firstMoney->multiply(1.33333);
+
+        self::assertSame(20007, $newMoney->amount());
+        self::assertSame(Currency::PLN, $newMoney->currency());
+    }
+
+    public function testDivideShouldDivideAmountAndKeepCurrency(): void
+    {
+        $firstMoney = Money::fromAmountAndCurrency(15000, Currency::PLN);
+
+        $newMoney = $firstMoney->divide(1.2);
+
+        self::assertSame(12500, $newMoney->amount());
+        self::assertSame(Currency::PLN, $newMoney->currency());
+
+        $firstMoney = Money::fromAmountAndCurrency(15005, Currency::PLN);
+
+        $newMoney = $firstMoney->divide(1.33);
+
+        self::assertSame(11282, $newMoney->amount());
+        self::assertSame(Currency::PLN, $newMoney->currency());
+
+        $firstMoney = Money::fromAmountAndCurrency(15005, Currency::PLN);
+
+        $newMoney = $firstMoney->divide(1.33333);
+
+        self::assertSame(11254, $newMoney->amount());
+        self::assertSame(Currency::PLN, $newMoney->currency());
+    }
+
+    public function testConvertShouldMultiplyMoneyAndChangeCurrency(): void
+    {
+        $firstMoney = Money::fromAmountAndCurrency(15000, Currency::USD);
+
+        $newMoney = $firstMoney->convert(Currency::PLN, 3.8);
+
+        self::assertSame(57000, $newMoney->amount());
+        self::assertSame(Currency::PLN, $newMoney->currency());
+    }
+}
